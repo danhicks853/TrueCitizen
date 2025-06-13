f54400b71eae4a7490f83786a99e8557c66dabfe
Dan <157068051+danhicks853@users.noreply.github.com>
2025-06-13 00:13:34 -0400

    Feat: Add test mode and improve slash commands

--- src/TrueCitizen/TrueCitizen.lua
+++ src/TrueCitizen/TrueCitizen.lua
@@ -27,11 +27,28 @@ frame:SetScript("OnEvent", function(self, event, ...)
     end
 end)
 
--- Slash command for testing
+-- Slash command for testing and control
 SLASH_TRUECITIZEN1 = "/truecitizen"
-SlashCmdList["TRUECITIZEN"] = function()
-    print("|cff33ff99TrueCitizen|r: " .. (inDalaran and "In Dalaran" or "Not in Dalaran"))
-    if inDalaran then
-        print("Will yell along with Rhonin when he gives his speech.")
+SLASH_TRUECITIZEN2 = "/tc"  -- Add an alias for convenience
+SlashCmdList["TRUECITIZEN"] = function(msg)
+    local cmd = strlower(strtrim(msg or ""))
+    
+    if cmd == "test" then
+        -- Test the yell functionality
+        local testMsg = "Testing TrueCitizen addon!"
+        SendChatMessage(testMsg, "YELL")
+        print("|cff33ff99TrueCitizen|r: Test yell sent!")
+    elseif cmd == "status" then
+        -- Show status information
+        print("|cff33ff99TrueCitizen|r: " .. (inDalaran and "In Dalaran" or "Not in Dalaran"))
+        if inDalaran then
+            print("Will yell along with Rhonin when he gives his speech.")
+        end
+    else
+        -- Show help
+        print("|cff33ff99TrueCitizen Commands:|r")
+        print("|cff33ff99/truecitizen test|r - Test the yell functionality")
+        print("|cff33ff99/truecitizen status|r - Show addon status")
+        print("|cff33ff99/truecitizen help|r - Show this help")
     end
 end
